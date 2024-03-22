7c049943f698d57c7ef3bacb9e16aa3446e49933
Ahmad Hanafi <ahanafi.id@gmail.com>
2024-03-23 06:16:18 +0700

    feat: install bootstrap css framework

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import * as Popper from '@popperjs/core'
+import 'bootstrap'
+
+window.Popper = Popper
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
